d47e6669e77ccb0a249b76370c8916e7ed97031e
Awesxme <manasvi.d007@gmail.com>
2026-02-23 14:16:09 +0000

    added tx2

--- client/index.test.ts
+++ client/index.test.ts
@@ -6,6 +6,7 @@ import {
   SystemProgram,
   Keypair,
   LAMPORTS_PER_SOL,
+  TransactionInstruction,
 } from "@solana/web3.js";
 
 test("one transfer", () => {
@@ -33,4 +34,33 @@ test("one transfer", () => {
   svm.sendTransaction(tx);
   const balanceAfter = svm.getBalance(dataAccount.publicKey);
   expect(balanceAfter).toBe(svm.minimumBalanceForRentExemption(BigInt(4)));
+
+  function doubleIt() {
+    // transaciton instruction 
+    const ix2 = new TransactionInstruction({
+      keys: [
+        { pubkey: dataAccount.publicKey, isSigner: false, isWritable: true },
+      ],
+      programId: contractPubkey,
+      data: Buffer.from("")
+    });
+    const tx2 = new Transaction();
+    const blockhash = svm.latestBlockhash();
+    tx2.recentBlockhash = blockhash;
+    tx2.add(ix2);
+    tx2.sign(payer);
+    svm.sendTransaction(tx2);
+    svm.expireBlockhash();
+  }
+  doubleIt();
+  doubleIt();
+  doubleIt();
+  doubleIt();
+
+  const newDataAccount = svm.getAccount(dataAccount.publicKey);
+
+  expect(newDataAccount?.data[0]).toBe(8);
+  expect(newDataAccount?.data[1]).toBe(0);
+  expect(newDataAccount?.data[2]).toBe(0);
+  expect(newDataAccount?.data[3]).toBe(0);
 });
